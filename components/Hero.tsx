497fbaed23bbacec65b5abe3a62229dcb3a4ec7b
Pixz1 <115209263+Pixz1@users.noreply.github.com>
2024-04-05 20:30:57 +1000

    Updated text on hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,13 +8,12 @@ const Hero = () => {
 
     return (
         <div className="hero">
-            <div className="flex-1 pt-36 pt-80-desktop padding-x">
+            <div className="flex-1 pt-36 pt-96-desktop padding-x">
                 <h1 className="hero__title">
-                    Find, book, or rent a car — quickly and easily!
+                    Find the perfect car for your getaway
                 </h1>
                 <p className="hero__subtitle">
-                    Streamline your car rental experience with our effortless
-                    boooking process.
+                    ...And let the good times roll.
                 </p>
                 <CustomButton
                     title="Explore Cars"
